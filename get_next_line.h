35d34a6a2899de2a5b9c17b6a826f837fe813d0e
t-pirinen <toimi.i.pirinen@gmail.com>
2025-05-10 23:47:24 +0300

    10.5 23:47

--- get_next_line.h
+++ get_next_line.h
@@ -21,7 +21,6 @@ char	*get_next_line(int fd);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
-void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
 
